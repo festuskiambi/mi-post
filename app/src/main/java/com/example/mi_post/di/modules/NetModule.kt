3d145225b2a9964676baf2d4eb8dec794496092c
festus <kiamfestoh@gmail.com>
2019-02-22 10:25:57 +0300

    setup retrofit

---
+++ app/src/main/java/com/example/mi_post/di/modules/NetModule.kt
@@ -0,0 +1,46 @@
+package com.example.mi_post.di.modules
+
+import com.example.mi_post.data.remote.ApiInterface
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.Retrofit.Builder
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+import javax.inject.Singleton
+
+/**
+ * Created by Festus Kiambi on 2/22/19.
+ */
+@Module
+class NetModule(val baseUrl: String){
+
+    @Provides
+    @Singleton
+    fun providesokHttpClient():OkHttpClient = OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(okHttpClient: OkHttpClient,moshi: Moshi): Retrofit{
+        return Builder().client(okHttpClient).baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesApiInterface(retrofit: Retrofit): ApiInterface = retrofit.create(
+        ApiInterface::class.java
+    )
+}
